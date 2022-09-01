4b14ef9ddceef7a0269493ddceca58246a2dd881
dcarroll9398 <dcarroll9398@yahoo.com>
2022-09-01 12:17:09 -0500

    Video-11

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Button, Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Rating from './Rating';
+
+export default function Product(props) {
+  const { product } = props;
+
+  return (
+    <Card>
+      <Link to={`/product/${product.slug}`}>
+        <img className="card-img-top" src={product.image} alt={product.name} />
+      </Link>
+      <Card.Body>
+        <Link to={`/product/${product.slug}`}>
+          <Card.Title>{product.name}</Card.Title>
+        </Link>
+        <Rating
+          rating={product.rating}
+          numReviews={product.numReviews}
+        ></Rating>
+        <Card.Text>${product.price}</Card.Text>
+        <Button type="button">Add to cart</Button>
+      </Card.Body>
+    </Card>
+  );
+}
