db8eaafee585797efee2f7aadeb6af9c37ea8769
dcarroll9398 <dcarroll9398@yahoo.com>
2022-09-05 22:18:13 -0500

    Video-17

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Button, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { Store } from '../Store';
 import Rating from './Rating';
+import axios from 'axios';
 
 export default function Product(props) {
   const { product } = props;
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const {
+    cart: { cartItems },
+  } = state;
+  const addToCartHandler = async (item) => {
+    const existItem = cartItems.find((x) => x._id === item._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${item._id}`);
+
+    if (data.countInStock < quantity) {
+      window.alert('Sorry. Product out of stock');
+      return;
+    }
+    ctxDispatch({ type: 'CART_ADD_ITEM', payload: { ...item, quantity } });
+  };
 
   return (
     <Card>
@@ -20,7 +37,15 @@ export default function Product(props) {
           numReviews={product.numReviews}
         ></Rating>
         <Card.Text>${product.price}</Card.Text>
-        <Button type="button">Add to cart</Button>
+        {product.countInStock === 0 ? (
+          <Button variant="light" disabled>
+            Out of stock
+          </Button>
+        ) : (
+          <Button onClick={() => addToCartHandler(product)} type="button">
+            Add to cart
+          </Button>
+        )}
       </Card.Body>
     </Card>
   );
---
+++ frontend/src/screens/SigninScreen.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SigninScreen() {
+  return (
+    <div className="small-container">
+      <h1>signin</h1>
+    </div>
+  );
+}
