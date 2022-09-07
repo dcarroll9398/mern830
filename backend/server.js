21d1d922d18563461ef31f487786ae2776f4b858
dcarroll9398 <dcarroll9398@yahoo.com>
2022-09-07 00:11:01 -0500

    Video-20

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import data from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import seedRouter from './routes/seedRoutes.js';
+import productRouter from './routes/productRoutes.js';
 
 dotenv.config();
 mongoose
@@ -14,10 +16,8 @@ mongoose
   });
 
 const app = express();
-
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
-});
+app.use('/api/seed', seedRouter);
+app.use('/api/products', productRouter);
 
 app.get('/api/products/slug/:slug', (req, res) => {
   const product = data.products.find((x) => x.slug === req.params.slug);
@@ -38,5 +38,5 @@ app.get('/api/products/:id', (req, res) => {
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Serve at http://localhost${port}`);
+  console.log(`Serve at http://localhost:${port}`);
 });
